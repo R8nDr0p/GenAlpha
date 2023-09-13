ffd73fd10c688b089cd254f50e84f31e5d0f7f1f
R8nDr0p <rainchrstn@gmail.com>
2023-09-13 16:10:17 +0800

    added userList and created a simple card-like component

--- frontend/src/user/components/UserItem.js
+++ frontend/src/user/components/UserItem.js
@@ -1,10 +1,22 @@
 import React from "react";
 import "./UserItem.css";
 
-function UserItem() {
+function UserItem({ key, id, placeCount, user, image }) {
   return (
     <div>
-      <h1>This is supposed to be card component</h1>
+      <li>
+        <div className="user-item__content">
+          <div className="user-item__image">
+            <img src={image} alt={` profile-picture__${user} `} />
+          </div>
+          <div className="user-item__info">
+            <h2>{user}</h2>
+            <h3>
+              {placeCount} {placeCount === 1 ? "Place" : "Places"}
+            </h3>
+          </div>
+        </div>
+      </li>
     </div>
   );
 }
--- frontend/src/user/pages/User.js
+++ frontend/src/user/pages/User.js
@@ -1,10 +1,20 @@
 import React from "react";
+import UserList from "../components/UserList";
 
 function User() {
+  const USERS = [
+    {
+      id: "u1",
+      name: "RainDrop",
+      places: "3",
+      img: "https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80",
+    },
+  ];
   return (
     <div>
       <h1>This is the user Page</h1>
       <p>this lists all active users of the application</p>
+      <UserList items={USERS} />
     </div>
   );
 }
