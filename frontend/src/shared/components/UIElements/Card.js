f4c10d215edb0bf7eeca1668106f0e1ef00f36c1
R8nDr0p <rainchrstn@gmail.com>
2023-10-22 12:40:03 +0800

    added shadow on the card component

--- frontend/src/shared/components/UIElements/Card.js
+++ frontend/src/shared/components/UIElements/Card.js
@@ -3,7 +3,7 @@ import React from "react";
 function Card(props) {
   return (
     <div
-      className={`card ${props.className} bg-slate-700 rounded-3xl p-4 text-amber-200 hover:text-slate-700 hover:bg-amber-400 transition-all duration-75 `}
+      className={`card ${props.className} bg-slate-700 rounded-3xl p-4 text-amber-200 hover:text-slate-700 hover:bg-amber-400 transition-all duration-75 shadow-md`}
       style={props.style}
     >
       {props.children}
