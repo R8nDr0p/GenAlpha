6fb7a362f0248d6dafe7cb3e35e2407fb8c30c3a
R8nDr0p <rainchrstn@gmail.com>
2023-10-28 18:34:53 +0800

    adding validation to the NewPlace.js with the Input.js

--- frontend/src/places/pages/NewPlace.js
+++ frontend/src/places/pages/NewPlace.js
@@ -3,12 +3,22 @@ import Input from "../../shared/components/FormElements/Input";
 
 function NewPlace() {
   return (
-      <>
-          <h1>New places page is here</h1>
-          <form className={"place-form list-none mx-auto p-4 w-[90%] max-w-[40rem] shadow-xl rounded bg-amber-100"}>
-              <Input element={'input'} type="text" label={'Title'} />
-          </form>
-      </>
+    <>
+      <h1>New places page is here</h1>
+      <form
+        className={
+          "place-form list-none mx-auto p-4 w-[90%] max-w-[40rem] shadow-xl rounded bg-amber-100"
+        }
+      >
+        <Input
+          element={"input"}
+          type="text"
+          label={"Title"}
+          validators={[]}
+          errorText={"Please enter a valid title"}
+        />
+      </form>
+    </>
   );
 }
 
