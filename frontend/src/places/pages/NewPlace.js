b2c128914dd4a9c22489fef68368acdfd340b535
R8nDr0p <rainchrstn@gmail.com>
2023-10-28 13:04:32 +0800

    creating a simple form for the NewPlace.js

--- frontend/src/places/pages/NewPlace.js
+++ frontend/src/places/pages/NewPlace.js
@@ -1,10 +1,14 @@
 import React from "react";
+import Input from "../../shared/components/FormElements/Input";
 
 function NewPlace() {
   return (
-    <div>
-      <h1>New places page is here</h1>
-    </div>
+      <>
+          <h1>New places page is here</h1>
+          <form className={"place-form list-none mx-auto p-4 w-[90%] max-w-[40rem] shadow-xl rounded bg-amber-100"}>
+              <Input element={'input'} type="text" label={'Title'} />
+          </form>
+      </>
   );
 }
 
